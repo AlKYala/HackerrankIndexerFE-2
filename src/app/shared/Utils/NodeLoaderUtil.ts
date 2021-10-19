6e6e783b425c6a75a7afa8010d0d857c97f1f806
aliyalama@googlemail.com <Gundam1979!>
2021-10-19 13:07:38 +0200

    added NodeLoaderUtil and tested

--- src/app/shared/Utils/NodeLoaderUtil.ts
+++ src/app/shared/Utils/NodeLoaderUtil.ts
@@ -1,5 +1,5 @@
 export class NodeLoaderUtil {
-  public loadJsFile(url: string, document: Document) {
+  public static loadJsFile(url: string, document: Document): void {
     let node = document.createElement('script');
     node.src = url;
     node.type = 'text/javascript';
@@ -7,10 +7,22 @@ export class NodeLoaderUtil {
     document.getElementsByTagName('head')[0].appendChild(node);
   }
 
-  public loadCSSFile(url: string, document: Document) {
+  public static loadCSSFile(url: string, document: Document): void {
     let node = document.createElement('link');
     node.href = url;
     node.rel='stylesheet';
     document.getElementsByTagName('head')[0].appendChild(node);
+  }
+
+  public static loadJSFiles(paths: string[], document: Document): void {
+    for(let path of paths) {
+      this.loadJsFile(path, document);
+    }
+  }
+
+  public static loadCSSFiles(paths: string[], document: Document) {
+    for(let path of paths) {
+      this.loadCSSFile(path, document);
+    }
   }
 }
