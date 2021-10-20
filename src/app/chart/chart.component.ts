a34f5492d2036073dad28f59e56eb0945257a845
aliyalama@googlemail.com <Gundam1979!>
2021-10-20 23:34:13 +0200

    chart complete

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,27 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {Label, MultiDataSet} from "ng2-charts";
 import {ChartType} from "chart.js";
+import {Subscription} from "rxjs";
+import {PLanguageService} from "../../shared/datamodels/PLanguage/service/PLanguageService";
+import {switchMap} from "rxjs/operators";
+import {Planguage} from "../../shared/datamodels/PLanguage/model/PLanguage";
+import {AnalyticsService} from "../../shared/services/AnalyticsService";
+import {UsageStatistics} from "../../shared/datamodels/Analytics/models/UsageStatistics";
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.css']
 })
-export class ChartComponent implements OnInit {
+export class ChartComponent implements OnInit, AfterViewInit {
 
-  public doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales', "AA", "BB", "CC", "DD", "EE", "FF", "GG", "HH"];
+  public doughnutChartLabels: Label[] = [];
   public doughnutChartData: MultiDataSet = [
-    [350, 450, 100, 99,99,99,99,99,99,99,99]
+    []
   ];
   public doughnutChartType: ChartType = 'doughnut';
   public loaded: boolean = false;
+  private subscriptions: Subscription[] = [];
+  private pLanguages: Planguage[] = [];
 
-  constructor() { }
+  constructor(private pLanguageService: PLanguageService,
+              private analyticsService: AnalyticsService) { }
 
   ngOnInit(): void {
+
   }
 
   private initData(): void {
+    const subscription: Subscription = this.pLanguageService
+      .findAll()
+      .pipe(switchMap((pLanguage: Planguage[]) => {
+        this.pLanguages = pLanguage;
+        return this.analyticsService.getUsagePercentagesOfPLanguages();
+      }))
+      .subscribe((data: UsageStatistics) => {
+        this.initUsagePercentages(data);
+        this.loaded = true;
+      });
+  }
 
+  private initUsagePercentages(statistics: UsageStatistics): void {
+    for(let i = 0; i < statistics.planguages.length; i++) {
+      this.doughnutChartLabels.push(statistics.planguages[i].language);
+      this.doughnutChartData[0].push(statistics.numberSubmissions[i]);
+    }
+  }
+
+  ngAfterViewInit(): void {
+    this.initData();
   }
 }
--- src/app/dataanalytics/dataanalytics.component.ts
+++ src/app/dataanalytics/dataanalytics.component.ts
@@ -60,19 +60,4 @@ export class DataanalyticsComponent implements OnInit {
       })
     this.subscriptions.push(subscription);
   }
-
-  public loadJsFile(url: string) {
-    let node = document.createElement('script');
-    node.src = url;
-    node.type = 'text/javascript';
-    console.log(node);
-    document.getElementsByTagName('head')[0].appendChild(node);
-  }
-
-  public loadCSSFile(url: string) {
-    let node = document.createElement('link');
-    node.href = url;
-    node.rel='stylesheet';
-    document.getElementsByTagName('head')[0].appendChild(node);
-  }
 }
