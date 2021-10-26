413a758507d49eabeeb543c834f97efab58ac138
aliyalama@googlemail.com <Gundam1979!>
2021-10-26 17:53:35 +0200

    add analytics insight to git

---
+++ src/app/analytics-insight/analytics-insight.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-analytics-insight',
+  templateUrl: './analytics-insight.component.html',
+  styleUrls: ['./analytics-insight.component.css']
+})
+export class AnalyticsInsightComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
